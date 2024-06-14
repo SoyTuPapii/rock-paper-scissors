f00a37578659d2fa200e80588e0aa805bed383e6
SoyTuPapii <168013459+SoyTuPapii@users.noreply.github.com>
2024-06-14 19:51:29 -0400

    Created getHumanChoice function

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-  // Your JavaScript goes here!
-  console.log("Hello, World!")
-  function getComputerChoice() {
-      let randomInt = Math.floor(Math.random() * 10);
-      console.log(randomInt)
-      return randomInt
+function getComputerChoice() {
+  let randomInt = Math.floor(Math.random() * 3);
+  if (randomInt === 1)  {
+    return "rock"
   }
+  else if (randomInt === 2)  {
+    return "paper"
+  }
+  else {
+    return "scissors"
+  }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Will you choose rock, paper, or scissors?")
+    return humanChoice
+}
+
   let computerChoice = getComputerChoice()
+  let humanChoice = getHumanChoice()
   console.log(computerChoice)
+  console.log(humanChoice)
+
+  let computerScore = 0
+  let humanScore = 0
