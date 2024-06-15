16175d6141aeec2a1fa7120a32510a02480570e7
SoyTuPapii <168013459+SoyTuPapii@users.noreply.github.com>
2024-06-14 20:27:02 -0400

    Finished playRound function

--- script.js
+++ script.js
@@ -22,14 +22,40 @@ function playRound(humanSelection, computerSelection) {
             console.log("It's a tie!")
         }
         else if (computerSelection === "paper") {
-            console.log("You lose! Paper beats rock.")
-            computerScore++
+            console.log("You lose! Paper beats rock.");
+            computerScore++;
         }
         else {
             console.log("You win! Rock smashes scissors.")
             humanScore++
         }
     
+    }
+    else if (humanSelection.toLowerCase() === "paper") {
+        if (computerSelection === "paper") {
+            console.log("It's a tie!");
+        }
+        else if (computerSelection === "scissors") {
+            console.log("You lose! Scissors cuts paper.");
+            computerScore++;
+        }
+        else {
+            console.log("You win! Paper covers rock.")
+            humanScore++
+        }
+    }
+    else if (humanSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "scissors") {
+            console.log("It's a tie!");
+        }
+        else if (computerSelection === "rock") {
+            console.log("You lose! Rock smashes scissors.");
+            computerScore++;
+        }
+        else {
+            console.log("You win! Scissors cuts paper.")
+            humanScore++
+        }
     }
 }
 
@@ -38,6 +64,6 @@ const humanSelection = getHumanChoice()
 console.log(computerSelection)
 console.log(humanSelection)
 
-  let computerScore = 0
-  let humanScore = 0
-  playRound(humanSelection, computerSelection)
+let computerScore = 0
+let humanScore = 0
+playRound(humanSelection, computerSelection)
