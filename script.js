4a9916e62d8613993aa9204143c6c816e1519228
SoyTuPapii <168013459+SoyTuPapii@users.noreply.github.com>
2024-06-14 20:12:07 -0400

    Created playGame function

--- script.js
+++ script.js
@@ -12,14 +12,32 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Will you choose rock, paper, or scissors?")
-    return humanChoice
+    let choice = prompt("Will you choose rock, paper, or scissors?")
+    return choice
 }
 
-  let computerChoice = getComputerChoice()
-  let humanChoice = getHumanChoice()
-  console.log(computerChoice)
-  console.log(humanChoice)
+function playRound(humanSelection, computerSelection) {
+    if (humanSelection.toLowerCase() === "rock") {
+        if (computerSelection === "rock") {
+            console.log("It's a tie!")
+        }
+        else if (computerSelection === "paper") {
+            console.log("You lose! Paper beats rock.")
+            computerScore++
+        }
+        else {
+            console.log("You win! Rock smashes scissors.")
+            humanScore++
+        }
+    
+    }
+}
+
+const computerSelection = getComputerChoice()
+const humanSelection = getHumanChoice()
+console.log(computerSelection)
+console.log(humanSelection)
 
   let computerScore = 0
   let humanScore = 0
+  playRound(humanSelection, computerSelection)
