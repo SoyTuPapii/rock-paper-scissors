a9f104a9206286c0e8156066fca39e987f38249b
SoyTuPapii <168013459+SoyTuPapii@users.noreply.github.com>
2024-07-02 14:57:24 -0400

    Added playGame function

--- script.js
+++ script.js
@@ -15,9 +15,30 @@ function getHumanChoice() {
     let choice = prompt("Will you choose rock, paper, or scissors?")
     return choice
 }
+function playGame() {
 
+    for (let i =0; i < 5; i++) {
+    const computerSelection = getComputerChoice()
+    const humanSelection = getHumanChoice()
+    console.log("Computer chose " + computerSelection)
+    console.log("Human chose " + humanSelection)
+    playRound(humanSelection, computerSelection)
+    }
+    console.log("Final score:")
+    console.log("Computer Score: " + computerScore)
+    console.log("Your score: " + humanScore)
+    if (humanScore > computerScore) {
+        console.log("You win!")
+    }
+    else if (computerScore > humanScore) {
+        console.log("Computer wins!")
+    }
+    else {
+        console.log("It's a tie!")
+    }
+}
 function playRound(humanSelection, computerSelection) {
-    if (humanSelection.toLowerCase() === "rock") {
+        if (humanSelection.toLowerCase() === "rock") {
         if (computerSelection === "rock") {
             console.log("It's a tie!")
         }
@@ -29,7 +50,7 @@ function playRound(humanSelection, computerSelection) {
             console.log("You win! Rock smashes scissors.")
             humanScore++
         }
-    
+        
     }
     else if (humanSelection.toLowerCase() === "paper") {
         if (computerSelection === "paper") {
@@ -59,11 +80,6 @@ function playRound(humanSelection, computerSelection) {
     }
 }
 
-const computerSelection = getComputerChoice()
-const humanSelection = getHumanChoice()
-console.log(computerSelection)
-console.log(humanSelection)
-
 let computerScore = 0
 let humanScore = 0
-playRound(humanSelection, computerSelection)
+playGame()
